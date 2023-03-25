f022e257c3ff1fcd940259855a20d32815765dd0
Shae Lorch <slorch@my.bcit.ca>
2023-03-24 23:32:42 -0700

    added search filters

---
+++ .history/components/Button_20230324213651.js
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+import { useRouter } from "next/router";
+import Image from "next/image";
+
+
+export default function MainButton ({ }){
+const r = useRouter();
+
+    return <div style={{display:'flex', marginTop:'10%', fontFamily:'bungee, sans-serif'}}>
+        <button onClick={() => {r.push('/findanagent')}
+        }
+        style={{display:'flex', flexDirection:'column', margin:10, alignItems:'center'
+        , justifyContent:'center', backgroundColor:'white', color:'#5AA1FD',
+         height:120, width: 120, borderRadius:15, border:'none' }}>
+            <Image src={'/findanagent.png'} width={100} height={70}></Image>
+            <div style={{fontSize:16}}>Find an Agent</div>
+        </button>
+        <button style={{display:'flex', flexDirection:'column', margin:10,
+         alignItems:'center', justifyContent:'center', backgroundColor:'white',
+          color:'#5AA1FD', height:120, width: 120, borderRadius:15, border:'none' }}>
+            <Image src={'/lightbulb.png'} width={100} height={90}></Image>
+            <div style={{fontSize:16}}>Tips</div>
+        </button>
+    </div>
+}
+
+
